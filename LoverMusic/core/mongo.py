f2ced26b26b56b8fcad02d6b860c09ec9dd8b638
TeamLover <123320837+TeamLover@users.noreply.github.com>
2025-02-01 21:51:27 +0530

    Update mongo.py

--- LoverMusic/core/mongo.py
+++ LoverMusic/core/mongo.py
@@ -1,34 +1,14 @@
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
-from pymongo import MongoClient
-from pyrogram import Client
+from motor.motor_asyncio import AsyncIOMotorClient
 
-import config
+from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://Legend:legendxd@cluster0.y9z7ufo.mongodb.net/?retryWrites=true&w=majority"
-
-
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning(
-        "Your bot will work on @shubhamsah1 mongo database..."
-    )
-    temp_client = Client(
-        "LoverMusic",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    pymongodb = _mongo_sync_.Anon
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Yukki
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
